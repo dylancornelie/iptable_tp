e7937ebf6f13def198c989342a2045f027b1a05b
Dylan Cornelie <higashu@hotmail.fr>
2022-02-26 19:24:31 +0100

    Marking packet of workstations for the router using TOS

--- workstation.sh
+++ workstation.sh
@@ -4,9 +4,11 @@ ip route add default via 192.168.2.1/24
 # DROPING packet send with a specific user
 # Not working with ping because always executing with root
 # but working with lynx
-# iptables -A OUTPUT -o eth0 -m owner --uid-owner 999 -j MARK --set-mark 1
-iptables -A OUTPUT -o eth0 -m owner --uid-owner 998 -j MARK --set-mark 1
-iptables -A OUTPUT -o eth9 -m mark --mark 1 -j DROP
+iptables -A OUTPUT -o eth0 -m owner --uid-owner 999 -j MARK --set-mark 1
+# iptables -A OUTPUT -o eth0 -m owner --uid-owner 998 -j MARK --set-mark 1
+# iptables -A OUTPUT -o eth0 -m mark --mark 1 -j DROP
 # iptables -t mangle -A POSTROUTING -o eth0 -m mark --mark 1 -j DSCP --set-dscp 1
+iptables -t mangle -A POSTROUTING -o eth0 -m mark --mark 1 -j TOS --set-tos 3
+
 
 
